ada5ba35a5057dd219af0ae27dc0dfffe923ae57
Omar <omaar_5@hotmail.com>
2021-10-23 19:31:09 +0200

    while Hover

--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -6,23 +7,41 @@ const Toppings = ({ addTopping, pizza }) => {
 
   return (
     <div className="toppings container">
-      
+
       <h3>Step 2: Choose Toppings</h3>
       <ul>
         {toppings.map(topping => {
           let spanClass = pizza.toppings.includes(topping) ? 'active' : '';
           return (
-            <li key={topping} onClick={() => addTopping(topping)}>
-              <span className={spanClass}>{ topping }</span>
-            </li>
+            <motion.li
+              key={topping}
+              onClick={() => addTopping(topping)}
+              whileHover={{
+                scale: 1.3,
+                color: "#f8e112",
+                originX: 0
+              }}
+              transition={{
+                type: "spring",
+                stiffness: 300
+              }}
+            >
+              <span className={spanClass}>{topping}</span>
+            </motion.li>
           )
         })}
       </ul>
 
       <Link to="/order">
-        <button>
+        <motion.button
+          whileHover={{
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgb(255, 255, 255)",
+            boxShadow: "0px 0px 8px rgb(255, 255, 255)"
+          }}
+        >
           Order
-        </button>
+        </motion.button>
       </Link>
 
     </div>
